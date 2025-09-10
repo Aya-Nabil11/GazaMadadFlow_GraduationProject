eb857cd94af60c66bf5ebf052b62f3e1a1928f38
Aya Nabil <alharazinaya@gmail.com>
2025-09-10 17:12:57 +0300

    commit4

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CitizenController;
 
-Route::get('/index', function () {
+Route::get('/', function () {
     return view('index'); 
 });
 
